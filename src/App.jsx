f05bfcdefb92b992c5ce07b620464ad15d124c49
Abhinav Mehta <144078110+A-b-h-i-n-a-v-1-9@users.noreply.github.com>
2025-07-22 13:03:13 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import RoutesPage from "./pages/RoutesPage";  // Import your RoutesPage
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/RoutesPage" element={<RoutesPage />} />
+
       </Routes>
     </Router>
   );
